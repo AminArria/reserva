150312d55434d593ec9ca21bf647704cbd10f688
Amin Arria <arria.amin@gmail.com>
2019-06-10 00:31:57 -0300

    Improved error handling in forms

--- lib/reserva_web/views/error_helpers.ex
+++ lib/reserva_web/views/error_helpers.ex
@@ -8,10 +8,15 @@ defmodule ReservaWeb.ErrorHelpers do
   @doc """
   Generates tag for inlined form input errors.
   """
-  def error_tag(form, field) do
-    Enum.map(Keyword.get_values(form.errors, field), fn error ->
-      content_tag(:span, translate_error(error), class: "help-block")
-    end)
+  # def error_tag(form, field) do
+  #   Enum.map(Keyword.get_values(form.errors, field), fn error ->
+  #     content_tag(:span, translate_error(error), class: "help-block")
+  #   end)
+  # end
+  def error_tag(nil) do
+  end
+  def error_tag({msg, _}) do
+    content_tag :span, msg, class: "help-block"
   end
 
   @doc """
